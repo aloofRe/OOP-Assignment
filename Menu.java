cbf60d228d448fcf1f6ba79c80593a37fc2e8f02
aloofRe <jlzh2007@gmail.com>
2026-04-11 13:25:52 +0800

    Backup before handoff

--- Menu.java
+++ Menu.java
@@ -1,3 +1,15 @@
-public class Menu {
-    
+import java.util.Scanner;
+
+
+public abstract class Menu {
+    protected MainManager mainManager;
+    protected Scanner scanner;
+
+
+    public Menu(MainManager mainManager, Scanner scanner) {
+        this.mainManager = mainManager;
+        this.scanner = scanner;
+    }
+
+    public abstract void start();
 }
